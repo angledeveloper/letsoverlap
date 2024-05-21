1ea18a5d3e8d6a9d1a3cb49321935cb5f2719f2f
siddhant <siddhantvij001@gmail.com>
2024-05-21 22:43:27 +0530

    seo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,29 @@
 import "./globals.css";
 import PreLayout from "./PreLayout";
 
+export const metadata = {
+  title: "LetsOverlap",
+  description:
+    "Overlap aims to host ambitious syndicate projects with up to the minute digital marketing solutions ",
+  generator: "letsOverlap",
+  applicationName: "letsOverlaps",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "digital marketing solutions",
+    "digital marketing agency",
+    "digital marketing agency in pune",
+    "best SEO services and solutions",
+    "SEO agency",
+    "leading SEO agency",
+    "best SEO agency",
+    "search engine optimisation",
+    "branding agency",
+    "marketing agency 2024",
+    "advertisement marketing",
+    "best advertising agency",
+  ],
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
